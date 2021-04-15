17aef9836b8e61411a34295606d575ecfa2cd40b
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-04-15 12:59:05 +0300

    The initial commit.

--- AwasomeBrouser/ViewController.swift
+++ AwasomeBrouser/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField.delegate = self
+        webView.navigationDelegate = self
+        
+        let homePage = "https://www.apple.com/"
+        let url = URL(string: homePage )
+        let request = URLRequest(url: url!)
+        
+        textField.text = homePage
+        
+        webView.load(request)
+        webView.allowsBackForwardNavigationGestures = true
     }
 
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    @IBAction func forwardButtonPressed(_ sender: UIButton) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+}
 
+extension ViewController: UITextFieldDelegate, WKNavigationDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if let urlString = textField.text {
+            if let url = URL(string: urlString) {
+                let request = URLRequest(url: url)
+                webView.load(request)
+            }
+        }
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        textField.text = webView.url?.absoluteString
+        
+        backButton.isEnabled = webView.canGoBack
+        forwardButton.isEnabled = webView.canGoForward
+    }
 }
 
